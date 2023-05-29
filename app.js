e6a895e61517b619c6119bacac40070fac5bd414
Patrick Gibson <gibsonp@rogers.com>
2023-05-29 19:22:04 -0400

    Adding basic UI for RPC

--- app.js
+++ app.js
@@ -45,47 +45,87 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-    let playerScore = 0;
-    let compScore = 0;
-
-    console.log("Welcome to Rock, Paper, Scissors!");
-    console.log("Let's play ".concat(NUM_GAMES, " games."));
     
-    for (let i=1; i<=NUM_GAMES; i++) {
-	let playerSelection = prompt('Type in "Rock", "Paper", or "Scissors".');
-	let computerSelection = getComputerChoice();
-
-	let result = playRound(playerSelection, computerSelection);
-
-	if (result != INVALID) {
-	    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-	}
-	
-	switch(result){
-	case WIN:
-	    console.log("Round ".concat(i, ": Win. ", playerSelection, " beats ", computerSelection, "."));
-	    playerScore += 1;
-	    break;
-	case LOSS:
-	    console.log("Round ".concat(i, ": Loss. ", playerSelection, " does not beat ", computerSelection, "."));
-	    compScore += 1;
-	    break;
-	case TIE:
-	    console.log("Round ".concat(i, ": Tie. You both selected ", computerSelection));
-	    break;
-	default:
-	    console.log("Pick a valid input, fucking idiot.");
-	    compScore += 1;
-	}
-    }
-
-    if (playerScore > compScore) {
-	console.log("You win!");
-    } else if (playerScore < compScore) {
-	console.log("You lose!");
-    } else {
-	console.log("Fuck, it's a tie.");
-    }
 }
+
+let playerChoice = "";
+/*
+// Grab buttons
+let rockButton = document.querySelector('#rock');
+let paperButton = document.querySelector('#paper');
+let scissorsButton = document.querySelector('#scissors');
+
+// Add event listeners to buttons and record the player's choice when the button is clicked
+rockButton.addEventListener('click', function(e) {
+    playerChoice = "rock";
+    console.log(playerChoice);
+});
+
+paperButton.addEventListener('click', function(e) {
+    playerChoice = "paper";
+    console.log(playerChoice);
+});
+
+scissorsButton.addEventListener('click', function(e) {
+    playerChoice = "scissors";
+    console.log(playerChoice);
+});
+*/
+
+// Test
+const buttons = document.querySelectorAll('.choice');
+const playAgain = document.querySelector('#replay');
+const picks = document.querySelector('#picks');
+const html = document.querySelector('#result');
+let computerChoice = "";
+let playerPts = 0;
+let computerPts = 0;
+let result = "";
+let resultText = "";
+
+buttons.forEach(button => button.addEventListener('click', function (e) {
+    let playerChoice = e.srcElement.getAttribute('data-choice');
+    computerChoice = getComputerChoice();
+    result = playRound(playerChoice, computerChoice);
+
+    let pickText = "You picked " + playerChoice + ". The computer picked " + computerChoice + ".";
+    
+    if (result == WIN) {
+	playerPts++;
+	if (playerPts == 5) {
+	    resultText = "Congratulations! You win!\nFinal Score: " + playerPts + "-" + computerPts;
+	} else {
+	    resultText = "Round Win. Score: " + playerPts + "-" + computerPts;
+	}
+    } else if (result == LOSS) {
+	computerPts++;
+	if (computerPts == 5) {
+	    resultText = "You lose, you stupid!\nFinal Score: " + playerPts + "-" + computerPts;
+	} else {
+	    resultText = "Round Loss. Score: " + playerPts + "-" + computerPts;
+	}
+    } else {
+	resultText = "Round Tie. Score: " + playerPts + "-" + computerPts;
+    }
+
+    // Disable choice buttons;
+    if (playerPts == 5 || computerPts == 5) {
+	buttons.forEach(button => button.disabled = true);
+	playAgain.removeAttribute('hidden');
+    }
+    
+    picks.innerHTML = pickText;
+    html.innerHTML = resultText;
+    
+}));
+
+playAgain.addEventListener('click', function (e) {
+    buttons.forEach(button => button.disabled = false);
+    playAgain.setAttribute('hidden', 'hidden');
+    playerPts = 0;
+    computerPts = 0;
+    picks.innerHTML = "";
+    html.innerHTML = "";
+});
 
 game()
